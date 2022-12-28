68d298f2542aecd8ab766e8e53bae41fbc9b1e74
youngmentor <olaleresuliton43@gmail.com>
2022-12-28 09:49:13 -0800

    updated

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -2,14 +2,14 @@ import '../App.css'
 import { useState } from "react"
 import { MdOutlineLightMode } from 'react-icons/md';
 import { MdOutlineNightlight } from 'react-icons/md';
-const Theme = ({theme, toggler}) => {
-   
+const Theme = ({ theme, toggler }) => {
+
     return (
         <div className='theme'>
-           {theme ? <MdOutlineLightMode onClick={() =>{toggler()} }
-          className='tog' /> :
-          <MdOutlineNightlight onClick={() => {toggler()}}
-            className='tog' color='black' />}
+            {theme ? <MdOutlineLightMode onClick={() => { toggler() }}
+                className='tog' /> :
+                <MdOutlineNightlight onClick={() => { toggler() }}
+                className='tog' color='black' />}
         </div>
     )
 }
